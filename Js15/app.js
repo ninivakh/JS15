570fa75007cd9dd22ab6a8e3c1137880bf387e87
ninivakh <120251081+ninivakh@users.noreply.github.com>
2023-07-16 19:24:59 +0400

    Add files via upload

---
+++ Js15/app.js
@@ -0,0 +1,8 @@
+function logout(){
+    var x = document.getElementById("cont");
+    x.style.display = "flex";
+}
+function canc(){
+    var y = document.getElementById("cont");
+    y.style.display = "none";
+}
